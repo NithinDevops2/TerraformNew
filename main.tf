1c2adc055afcfeeadcc61edfd541452a3a289930
nithin1305 <nithin1305@hotmail.com>
2023-07-05 17:15:05 +0530

    Updated 2

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_route_table" "my_route_table" {
   vpc_id = aws_vpc.my_vpc.id
 
   route {
-    cidr_block = "172.20.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my_igw.id
   }
 
